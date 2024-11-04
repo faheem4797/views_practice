f2382d9e5919bdaa69c8d3d19efcd28369ba511e
Faheem <mfaheemamin4797@gmail.com>
2024-11-05 04:45:27 +0500

    Intents Added

--- app/src/main/java/com/example/viewspractice/MainActivity.kt
+++ app/src/main/java/com/example/viewspractice/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.viewspractice
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -14,10 +17,19 @@ class MainActivity : AppCompatActivity() {
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val explicitButton = findViewById<Button>(R.id.btnExplicitButton)
+
+        explicitButton.setOnClickListener{
+            val explicitIntent = Intent(this, SecondActivity::class.java)
+            startActivity(explicitIntent)
+            finish()
+        }
+
+        val url = "https://www.google.com"
+        val implicitButton = findViewById<Button>(R.id.btnImplicitButton)
+        implicitButton.setOnClickListener {
+            val implicitIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity((implicitIntent))
         }
     }
 }
