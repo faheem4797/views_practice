79a5794c7b2ad6dd934b2c32eb16410781a7a369
Faheem <mfaheemamin4797@gmail.com>
2024-11-19 18:06:16 +0500

    Video View Done

--- app/src/main/java/com/example/viewspractice/MainActivity.kt
+++ app/src/main/java/com/example/viewspractice/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.viewspractice
 
 import android.media.MediaPlayer
+import android.net.Uri
+import android.net.Uri.parse
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,26 +10,20 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.MediaController
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.w3c.dom.Text
+import java.net.URI
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-
-    private var mediaPlayer: MediaPlayer? = null
-    private  lateinit var seekBar: SeekBar
-    private lateinit var runnable: Runnable
-    private  lateinit var handler: Handler
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,72 +32,17 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
 
-        seekBar = findViewById(R.id.sbClap)
-        handler = Handler(Looper.getMainLooper())
-
-        val btnPlay = findViewById<FloatingActionButton>(R.id.fabPlay)
-        val btnPause = findViewById<FloatingActionButton>(R.id.fabPause)
-        val btnStop = findViewById<FloatingActionButton>(R.id.fabStop)
-        val tvPlayed = findViewById<TextView>(R.id.tvPlayed)
-        val tvDue = findViewById<TextView>(R.id.tvDue)
-
-        btnPlay.setOnClickListener {
-            if(mediaPlayer == null){
-                mediaPlayer = MediaPlayer.create(this, R.raw.clap)
-                initializeSeekBar()
-            }
-            mediaPlayer?.start()
-        }
-        btnPause.setOnClickListener {
-            mediaPlayer?.pause()
-        }
-        btnStop.setOnClickListener {
-            mediaPlayer?.stop()
-            mediaPlayer?.reset()
-            mediaPlayer?.release()
-            mediaPlayer = null
-            handler.removeCallbacks(runnable)
-            seekBar.progress = 0
-
-            tvPlayed.text = ""
-            tvDue.text = ""
-        }
-    }
-
-    private fun initializeSeekBar(){
-        seekBar.setOnSeekBarChangeListener(object  : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if(fromUser) mediaPlayer?.seekTo(progress)
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                TODO("Not yet implemented")
-            }
-
-        }
-        )
+        val uri: Uri = parse("android.resource://" + packageName + "/" + R.raw.light)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
 
 
-        val tvPlayed = findViewById<TextView>(R.id.tvPlayed)
-        val tvDue = findViewById<TextView>(R.id.tvDue)
-        seekBar.max = mediaPlayer!!.duration
-        runnable = Runnable {
-            seekBar.progress = mediaPlayer!!.currentPosition
-            val playedTime = mediaPlayer!!.currentPosition/1000
-            tvPlayed.text = "$playedTime sec"
-
-            val duration = mediaPlayer!!.duration/1000
-            val dueTime = duration - playedTime
-            tvDue.text = "$dueTime sec"
-
-            handler.postDelayed(runnable, 100)
-        }
-        handler.postDelayed(runnable,100)
     }
 
 
