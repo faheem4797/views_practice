09f0ab49707b5e5e4be4d03c17b5902201d37a2a
Faheem <mfaheemamin4797@gmail.com>
2024-11-22 23:01:40 +0500

    ViewBinding Done

--- app/src/main/java/com/example/viewspractice/MainActivity.kt
+++ app/src/main/java/com/example/viewspractice/MainActivity.kt
@@ -3,150 +3,40 @@ package com.example.viewspractice
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
-import com.example.viewspractice.db.Student
-import com.example.viewspractice.db.StudentDao
-import com.example.viewspractice.db.StudentDatabase
+import com.example.viewspractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var etName: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var btnSave: Button
-    private lateinit var btnClear: Button
-    private lateinit var rvStudents: RecyclerView
-    private lateinit var rvAdapter : StudentRecyclerViewAdapter
-
-    private  lateinit var studentViewModel: StudentViewModel
-
-    private lateinit var currentStudent: Student
-
-    private var isListItemClicked = false
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
 
         installSplashScreen()
-        setContentView(R.layout.activity_main)
 
-        val studentDao = StudentDatabase.getInstance(application).studentDao()
-        val studentViewModelFactory = StudentViewModelFactory(studentDao)
-        studentViewModel = ViewModelProvider(this, studentViewModelFactory).get(StudentViewModel::class.java)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
 
-        etName = findViewById(R.id.etName)
-        etEmail = findViewById(R.id.etEmail)
-        btnSave = findViewById(R.id.btnSave)
-        btnClear = findViewById(R.id.btnClear)
-        rvStudents = findViewById(R.id.rvStudents)
+        setContentView(view)
 
-        btnSave.setOnClickListener {
-            if(isListItemClicked){
-                updateStudentData()
-                turnBtnsBackToSaveAndClear()
+        binding.apply{
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello $name!"
+                etName.setText("")
             }
-            else{
-                saveStudentData()
-                clearInputs()
-            }
-
         }
 
-        btnClear.setOnClickListener {
-            if(isListItemClicked){
-                deleteStudentData()
-                turnBtnsBackToSaveAndClear()
-            }else{
-                clearInputs()
-            }
 
-        }
 
-        initRecyclerView()
 
 
     }
 
-    private fun clearInputs(){
-        etName.setText("")
-        etEmail.setText("")
-    }
-
-    private fun turnBtnsBackToSaveAndClear(){
-        btnSave.text = "Save"
-        btnClear.text = "Clear"
-        isListItemClicked = false
-        clearInputs()
-    }
-
-    private fun saveStudentData(){
-        studentViewModel.insertStudent(
-            Student(
-                0,
-                etName.text.toString(),
-                etEmail.text.toString(),
-            )
-        )
-    }
-
-    private fun updateStudentData(){
-        studentViewModel.updateStudent(
-            Student(
-                currentStudent.id,
-                etName.text.toString(),
-                etEmail.text.toString(),
-            )
-        )
-    }
-    private fun deleteStudentData(){
-        studentViewModel.deleteStudent(
-            Student(
-                currentStudent.id,
-                etName.text.toString(),
-                etEmail.text.toString(),
-            )
-        )
-    }
-
-
-
-    private fun initRecyclerView(){
-        rvStudents.layoutManager = LinearLayoutManager(this)
-        rvAdapter = StudentRecyclerViewAdapter{
-            itemListClicked(it)
-        }
-        rvStudents.adapter = rvAdapter
-        displayStudentsList()
-    }
-
-    private fun displayStudentsList(){
-        studentViewModel.students.observe(this, {
-           rvAdapter.setStudentList(it)
-            rvAdapter.notifyDataSetChanged()
-        })
-    }
-
-    private fun itemListClicked(student: Student){
-//        Toast.makeText(this, "Clicked Student name is ${student.name}", Toast.LENGTH_SHORT).show()
-
-        if(!isListItemClicked){
-            currentStudent = student
-            btnSave.text = "Update"
-            btnClear.text = "Delete"
-            isListItemClicked = true
-            etName.setText(student.name)
-            etEmail.setText(student.email)
-        }else{
-           turnBtnsBackToSaveAndClear()
-        }
-
-    }
 
 }
 
