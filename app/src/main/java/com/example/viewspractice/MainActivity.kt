9d25e87523570fae28b95227851160c9031ffeb6
Faheem <mfaheemamin4797@gmail.com>
2024-11-22 23:40:00 +0500

    Room Student DB app done with viewbing now

--- app/src/main/java/com/example/viewspractice/MainActivity.kt
+++ app/src/main/java/com/example/viewspractice/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.viewspractice
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.viewspractice.databinding.ActivityMainBinding
+import com.example.viewspractice.databinding.ListItemBinding
+import com.example.viewspractice.db.Student
+import com.example.viewspractice.db.StudentDatabase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
+    private lateinit var rvAdapter : StudentRecyclerViewAdapter
+
+    private  lateinit var studentViewModel: StudentViewModel
+
+    private lateinit var currentStudent: Student
+
+    private var isListItemClicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,24 +30,124 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
 
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding.apply{
-            btnSubmit.setOnClickListener {
-                val name = etName.text.toString()
-                tvMessage.text = "Hello $name!"
-                etName.setText("")
+        val studentDao = StudentDatabase.getInstance(application).studentDao()
+        val studentViewModelFactory = StudentViewModelFactory(studentDao)
+        studentViewModel = ViewModelProvider(this, studentViewModelFactory).get(StudentViewModel::class.java)
+
+
+
+        binding.apply {
+            btnSave.setOnClickListener {
+                if(isListItemClicked){
+                    updateStudentData()
+                    turnBtnsBackToSaveAndClear()
+                }
+                else{
+                    saveStudentData()
+                    clearInputs()
+                }
             }
+
+            btnClear.setOnClickListener {
+                if(isListItemClicked){
+                    deleteStudentData()
+                    turnBtnsBackToSaveAndClear()
+                }else{
+                    clearInputs()
+                }
+            }
+
         }
-
-
-
+        initRecyclerView()
 
 
     }
 
+    private fun clearInputs(){
+        binding.apply {
+            etName.setText("")
+            etEmail.setText("")
+        }
+    }
+
+    private fun turnBtnsBackToSaveAndClear(){
+        binding.apply {
+            btnSave.text = "Save"
+            btnClear.text = "Clear"
+            isListItemClicked = false
+            clearInputs()
+        }
+    }
+
+    private fun saveStudentData(){
+        binding.apply {
+            studentViewModel.insertStudent(
+                Student(
+                    0,
+                    etName.text.toString(),
+                    etEmail.text.toString(),
+                )
+            )
+        }
+    }
+
+    private fun updateStudentData(){
+        binding.apply {
+        studentViewModel.updateStudent(
+            Student(
+                currentStudent.id,
+                etName.text.toString(),
+                etEmail.text.toString(),
+            )
+        )}
+    }
+    private fun deleteStudentData(){
+        binding.apply {
+        studentViewModel.deleteStudent(
+            Student(
+                currentStudent.id,
+                etName.text.toString(),
+                etEmail.text.toString(),
+            )
+        )}
+    }
+
+
+
+    private fun initRecyclerView(){
+        binding.rvStudents.layoutManager = LinearLayoutManager(this)
+        rvAdapter = StudentRecyclerViewAdapter{
+            itemListClicked(it)
+        }
+        binding.rvStudents.adapter = rvAdapter
+        displayStudentsList()
+    }
+
+    private fun displayStudentsList(){
+        studentViewModel.students.observe(this, {
+            rvAdapter.setStudentList(it)
+            rvAdapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun itemListClicked(student: Student){
+
+        if(!isListItemClicked){
+            binding.apply {
+                currentStudent = student
+                btnSave.text = "Update"
+                btnClear.text = "Delete"
+                isListItemClicked = true
+                etName.setText(student.name)
+                etEmail.setText(student.email)
+            }
+        }else{
+            turnBtnsBackToSaveAndClear()
+        }
+
+    }
 
 }
-
