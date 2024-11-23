c072a71cd5fc2003613f37b7a5865173962af8ac
Faheem <mfaheemamin4797@gmail.com>
2024-11-23 23:29:28 +0500

    First Service Created

--- app/src/main/java/com/example/viewspractice/MainActivity.kt
+++ app/src/main/java/com/example/viewspractice/MainActivity.kt
@@ -1,27 +1,16 @@
 package com.example.viewspractice
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.viewspractice.MyBackgroundService.Companion.TAG
 import com.example.viewspractice.databinding.ActivityMainBinding
-import com.example.viewspractice.databinding.ListItemBinding
-import com.example.viewspractice.db.Student
-import com.example.viewspractice.db.StudentDatabase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-
-    private lateinit var rvAdapter : StudentRecyclerViewAdapter
-
-    private  lateinit var studentViewModel: StudentViewModel
-
-    private lateinit var currentStudent: Student
-
-    private var isListItemClicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,124 +19,23 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
-        val studentDao = StudentDatabase.getInstance(application).studentDao()
-        val studentViewModelFactory = StudentViewModelFactory(studentDao)
-        studentViewModel = ViewModelProvider(this, studentViewModelFactory).get(StudentViewModel::class.java)
-
-
-
-        binding.apply {
-            btnSave.setOnClickListener {
-                if(isListItemClicked){
-                    updateStudentData()
-                    turnBtnsBackToSaveAndClear()
-                }
-                else{
-                    saveStudentData()
-                    clearInputs()
-                }
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        
+        binding.apply { 
+            btnStart.setOnClickListener{
+                Log.i(TAG, " Starting Service")
+                startService(serviceIntent)
             }
-
-            btnClear.setOnClickListener {
-                if(isListItemClicked){
-                    deleteStudentData()
-                    turnBtnsBackToSaveAndClear()
-                }else{
-                    clearInputs()
-                }
+            btnStop.setOnClickListener{
+                Log.i(TAG, " Stopping Service")
+                stopService(serviceIntent)
             }
-
-        }
-        initRecyclerView()
-
-
-    }
-
-    private fun clearInputs(){
-        binding.apply {
-            etName.setText("")
-            etEmail.setText("")
-        }
-    }
-
-    private fun turnBtnsBackToSaveAndClear(){
-        binding.apply {
-            btnSave.text = "Save"
-            btnClear.text = "Clear"
-            isListItemClicked = false
-            clearInputs()
-        }
-    }
-
-    private fun saveStudentData(){
-        binding.apply {
-            studentViewModel.insertStudent(
-                Student(
-                    0,
-                    etName.text.toString(),
-                    etEmail.text.toString(),
-                )
-            )
-        }
-    }
-
-    private fun updateStudentData(){
-        binding.apply {
-        studentViewModel.updateStudent(
-            Student(
-                currentStudent.id,
-                etName.text.toString(),
-                etEmail.text.toString(),
-            )
-        )}
-    }
-    private fun deleteStudentData(){
-        binding.apply {
-        studentViewModel.deleteStudent(
-            Student(
-                currentStudent.id,
-                etName.text.toString(),
-                etEmail.text.toString(),
-            )
-        )}
-    }
-
-
-
-    private fun initRecyclerView(){
-        binding.rvStudents.layoutManager = LinearLayoutManager(this)
-        rvAdapter = StudentRecyclerViewAdapter{
-            itemListClicked(it)
-        }
-        binding.rvStudents.adapter = rvAdapter
-        displayStudentsList()
-    }
-
-    private fun displayStudentsList(){
-        studentViewModel.students.observe(this, {
-            rvAdapter.setStudentList(it)
-            rvAdapter.notifyDataSetChanged()
-        })
-    }
-
-    private fun itemListClicked(student: Student){
-
-        if(!isListItemClicked){
-            binding.apply {
-                currentStudent = student
-                btnSave.text = "Update"
-                btnClear.text = "Delete"
-                isListItemClicked = true
-                etName.setText(student.name)
-                etEmail.setText(student.email)
-            }
-        }else{
-            turnBtnsBackToSaveAndClear()
         }
 
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/viewspractice/MyBackgroundService.kt
@@ -0,0 +1,28 @@
+package com.example.viewspractice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyBackgroundService : Service() {
+    init {
+        Log.i(TAG, "Service has been created")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(TAG, "Service is starting.....")
+        return START_STICKY
+    }
+
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        Log.i(TAG, "Destroying.....")
+        super.onDestroy()
+    }
+
+    companion object {
+        const val TAG = "pheeem"
+    }
+}
