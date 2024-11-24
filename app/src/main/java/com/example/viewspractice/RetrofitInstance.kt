50c3e76d5fff8783e0a4bc1592ca38509765ff91
Faheem <mfaheemamin4797@gmail.com>
2024-11-25 01:38:45 +0500

    Retrofit Done

--- app/src/main/java/com/example/viewspractice/RetrofitInstance.kt
+++ app/src/main/java/com/example/viewspractice/RetrofitInstance.kt
@@ -3,15 +3,29 @@ package com.example.viewspractice
 import retrofit2.Retrofit
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 class RetrofitInstance {
     companion object {
-        val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        private val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        private val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(25, TimeUnit.SECONDS)
+        }.build()
+
         fun getRetrofit(): Retrofit {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
